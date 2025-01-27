7fbeac8ecab62a5782af5e2e144459f05259010b
Francisco Zorat <franciscozorat@gmail.com>
2025-01-27 09:03:30 -0300

    feat: update test to use fetchMatches and fetchLeagues functions for fetching data

--- test.ts
+++ test.ts
@@ -1,9 +1,17 @@
-import { fetchPromiedos } from "./src";
+import { fetchMatches, fetchLeagues } from "./src";
 
 const test = async () => {
   try {
-    const matches = await fetchPromiedos({ mode: "yesterday" });
+    console.log('Fetching matches...');
+    const matches = await fetchMatches({ mode: "yesterday" });
     console.log(matches);
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    console.log('Fetching leagues...');
+    const leagues = await fetchLeagues({ mode: "yesterday" });
+    console.log(leagues);
   } catch (error) {
     console.error(error);
   }
