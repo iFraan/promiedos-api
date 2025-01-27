4b709fcba70c80815c101c2b90d8ecf09fd209cb
Francisco Zorat <franciscozorat@gmail.com>
2025-01-27 08:33:13 -0300

    feat: extend Match type with additional properties

--- src/types/internal.ts
+++ src/types/internal.ts
@@ -5,10 +5,34 @@ export type Match = {
         name: string;
         score: string;
         logo?: string;
+        short_name?: string;
+        id?: string;
+        red_cards?: number;
+        country_id?: string;
     };
     team2: {
         name: string;
         score: string;
         logo?: string;
+        short_name?: string;
+        id?: string;
+        red_cards?: number;
+        country_id?: string;
     };
+    id?: string;
+    stage_round_name?: string;
+    status?: {
+        enum: number;
+        name: string;
+        short_name: string;
+    };
+    tv_networks?: {
+        id: string;
+        name: string;
+    }[];
+    odds?: {
+        name: string;
+        value: number;
+        trend: number;
+    }[];
 }
